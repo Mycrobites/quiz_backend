2f4727141d5af34fa6286a8ba346adf5b7ebcb0e
abhishek-st <ankit2632000@gmail.com>
2021-03-15 15:54:48 +0530

    models added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
+import jsonfield
 # Create your models here.
+
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    time = models.DateTimeField(auto_now_add=True, blank=True)
+    desc = models.TextField()
+    endtime = models.DateTimeField()
+
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question = models.TextField()
+    correct_marks = models.SmallIntegerField()
+    negative_marks = models.SmallIntegerField()
+    answer = models.PositiveSmallIntegerField()
+    option = jsonfield.JSONField()
+    text = models.TextField()
+
+class QuizAssign(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)

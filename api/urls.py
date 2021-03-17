259878792334a1f3b39eb0e1d43355a81c6b0531
Abhinay <abhinaybathina2@gmail.com>
2021-03-17 16:32:30 +0530

    APIs added

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,12 @@ from .views import *
 
 
 urlpatterns = [
-    path("getQuiz/<slug:quiz_id>", QuizView.as_view(), name="get-quiz"),
-    path("createQuiz", QuizCreateView.as_view(), name="create-quiz"),
-    path("getResponse/<slug:quiz_id>/<int:user_id>", QuizGetResponseView.as_view(), name="get-response"),
-    path("createResponse", QuizCreateResponseView.as_view(), name="create-response"),
-    path("getQuizMarks/<slug:quiz_id>/<int:user_id>", QuizMarksView.as_view(), name="get-quiz-marks"),
+    path("create-quiz", QuizCreateView.as_view(), name="create-quiz"),
+    path("get-quiz/<slug:quiz_id>", QuizView.as_view(), name="get-quiz"),
+    path("edit-quiz/<slug:quiz_id>", QuizEditView.as_view(), name="edit-quiz"),
+    path("create-question", QuizQuestionCreateView.as_view(), name="create-question"),
+    path("edit-question/<slug:question_id>", QuizQuestionEditView.as_view(), name="edit-question"),
+    path("create-response", QuizCreateResponseView.as_view(), name="create-response"),
+    path("get-response/<slug:quiz_id>/<int:user_id>", QuizGetResponseView.as_view(), name="get-response"),
+    path("get-quiz-marks/<slug:quiz_id>/<int:user_id>", QuizMarksView.as_view(), name="get-quiz-marks"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
 from .serializers import QuizSerializer, QuestionSerializer, QuizAssignSerializer
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from .models import Quiz, Question, QuizAssign
+from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist
 import json
 # Create your views here.
 
@@ -12,13 +14,16 @@ class QuizView(GenericAPIView):
 
     def get(self, request, quiz_id):
         result = {}
-        quiz = get_object_or_404(Quiz, id=quiz_id)
-        serializer = self.serializer_class(quiz)
-        questions = Question.objects.filter(quiz=quiz)
-        ques_serializer = QuestionSerializer(questions, many=True)
-        result['quiz_details'] = serializer.data
-        result['quiz_questions'] = ques_serializer.data
-        return Response(result)
+        try:
+            quiz = Quiz.objects.get(id=quiz_id)
+            serializer = self.serializer_class(quiz)
+            questions = Question.objects.filter(quiz=quiz)
+            ques_serializer = QuestionSerializer(questions, many=True)
+            result['quiz_details'] = serializer.data
+            result['quiz_questions'] = ques_serializer.data
+            return Response(result)
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Quiz not found with the given id"})
 
 
 class QuizCreateView(GenericAPIView):
@@ -30,6 +35,63 @@ class QuizCreateView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class QuizEditView(GenericAPIView):
+    serializer_class = QuizSerializer
+
+    def put(self, request, quiz_id):
+        try:
+            quiz = Quiz.objects.get(id=quiz_id)
+            data = request.data
+            serializer = self.serializer_class(quiz, data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Quiz not found with the given id"})
+
+    def delete(self, request, quiz_id):
+        try:
+            quiz = Quiz.objects.get(id=quiz_id)
+            quiz.delete()
+            return Response({"message": "Quiz deleted successfully"})
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Quiz not found with the given id"})
+
+
+class QuizQuestionCreateView(GenericAPIView):
+    serializer_class = QuestionSerializer
+
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class QuizQuestionEditView(GenericAPIView):
+    serializer_class = QuestionSerializer
+
+    def put(self, request, question_id):
+        try:
+            question = Question.objects.get(id=question_id)
+            data = request.data
+            serializer = self.serializer_class(question, data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Question not found with the given id"})
+
+    def delete(self, request, question_id):
+        try:
+            question = Question.objects.get(id=question_id)
+            question.delete()
+            return Response({"message": "Question deleted successfully"})
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Question not found with the given id"})
 
 
 class QuizCreateResponseView(GenericAPIView):
@@ -47,35 +109,57 @@ class QuizGetResponseView(GenericAPIView):
     serializer_class = QuizAssignSerializer
 
     def get(self, request, quiz_id, user_id):
-        quiz = get_object_or_404(QuizAssign, quiz=quiz_id, user=user_id)
-        serializer = self.serializer_class(quiz)
-        return Response(serializer.data)
+        try:
+            Quiz.objects.get(id=quiz_id)
+            try:
+                User.objects.get(id=user_id)
+                try:
+                    quiz_assign = QuizAssign.objects.get(quiz=quiz_id, user=user_id)
+                    serializer = self.serializer_class(quiz_assign)
+                    return Response(serializer.data)
+                except ObjectDoesNotExist:
+                    raise ValidationError({"message": "Quiz was not attempted by the student with given user id"})
+            except ObjectDoesNotExist:
+                raise ValidationError({"message": "User not found with the given id"})
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Quiz not found with the given id"})
 
 
 class QuizMarksView(GenericAPIView):
     serializer_class = QuizAssignSerializer
 
     def get(self, request, quiz_id, user_id):
-        quiz_assign = get_object_or_404(QuizAssign, quiz=quiz_id, user=user_id)
-        serializer = self.serializer_class(quiz_assign)
-        response = serializer.data['response']
-        response = response.replace("'", '"')
-        res_dict = json.loads(response)
-        quiz = quiz_assign.quiz
-        questions = Question.objects.filter(quiz=quiz)
-        marks = 0
-        for i in range(len(questions)):
-            if questions[i].answer is None:
-                if questions[i].text == res_dict[str(questions[i].id)]:
-                    marks += questions[i].correct_marks
-                else:
-                    marks += questions[i].negative_marks
-            elif questions[i].text == "":
-                if str(questions[i].answer) == res_dict[str(questions[i].id)]:
-                    marks += questions[i].correct_marks
-                else:
-                    marks += questions[i].negative_marks
-            elif questions[i].answer == "" and questions[i].text == "":
-                marks += 0
-        QuizAssign.objects.filter(quiz=quiz_id, user=user_id).update(marks=marks)
-        return Response({"quiz": quiz.id, "user": user_id, "marks": marks})
+        try:
+            Quiz.objects.get(id=quiz_id)
+            try:
+                User.objects.get(id=user_id)
+                try:
+                    quiz_assign = QuizAssign.objects.get(quiz=quiz_id, user=user_id)
+                    serializer = self.serializer_class(quiz_assign)
+                    response = serializer.data['response']
+                    response = response.replace("'", '"')
+                    res_dict = json.loads(response)
+                    quiz = quiz_assign.quiz
+                    questions = Question.objects.filter(quiz=quiz)
+                    marks = 0
+                    for i in range(len(questions)):
+                        if questions[i].answer is None:
+                            if questions[i].text == res_dict[str(questions[i].id)]:
+                                marks += questions[i].correct_marks
+                            else:
+                                marks += questions[i].negative_marks
+                        elif questions[i].text == "":
+                            if str(questions[i].answer) == res_dict[str(questions[i].id)]:
+                                marks += questions[i].correct_marks
+                            else:
+                                marks += questions[i].negative_marks
+                        elif questions[i].answer == "" and questions[i].text == "":
+                            marks += 0
+                    QuizAssign.objects.filter(quiz=quiz_id, user=user_id).update(marks=marks)
+                    return Response({"quiz": quiz.id, "user": user_id, "marks": marks})
+                except ObjectDoesNotExist:
+                    raise ValidationError({"message": "Quiz was not attempted by the student with given user id"})
+            except ObjectDoesNotExist:
+                raise ValidationError({"message": "User not found with the given id"})
+        except ObjectDoesNotExist:
+            raise ValidationError({"message": "Quiz not found with the given id"})

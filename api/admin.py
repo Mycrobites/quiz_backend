2aa4041705be092b066a17f6a3eb2c4dd8ac6cb5
Rajat Shrivastava <rajat123468@gmail.com>
2021-03-28 20:17:08 +0530

    added quiz filter

--- api/admin.py
+++ api/admin.py
@@ -8,7 +8,7 @@ admin.site.register(Quiz)
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_filter = ["subject_tag", "topic_tag", "subtopic_tag", "dificulty_tag", "skill"]
+    list_filter = ["quiz","subject_tag", "topic_tag", "subtopic_tag", "dificulty_tag", "skill"]
     list_display = ["quiz", "question", "correct_marks", "negative_marks", "answer"]
 
 

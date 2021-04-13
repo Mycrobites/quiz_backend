a4f9cc9d81b70996cf9f44ac68c0a2124a557d59
Rajat <rajat123468@gmail.com>
2021-04-13 19:20:28 +0000

    Added Actions Runner

--- runserver.py
+++
@@ -1,9 +0,0 @@
-  
-from waitress import serve
-
-from quiz.wsgi import application
-# documentation: https://docs.pylonsproject.org/projects/waitress/en/stable/api.html
-
-if __name__ == '__main__':
-    print("Server running at https://api.progressiveminds.in")
-    serve(application, host = 'localhost', port='8080')
